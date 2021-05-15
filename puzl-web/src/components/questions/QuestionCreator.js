d0369850384ef357d65118b9335f0aabea6c5184
Rahtoken <23293949+Rahtoken@users.noreply.github.com>
2021-05-15 19:47:48 +0530

    [WIP]Refactor update logic. (#8)
    * Refactor update logic.
    
    * Disallow duplicate question creation.
    
    * Disallow duplicate question update.
    
    * Go to view mode on creation.

---
+++ puzl-web/src/components/questions/QuestionCreator.js
@@ -0,0 +1,12 @@
+export const doesSameQuestionExists = (newQuestion, questions) =>
+  questions.find((question) => question === newQuestion) !== undefined;
+
+export const isQuestionValid = (newQuestion) => newQuestion.trim() !== "";
+
+export const isOptionValid = (option) => option.trim() !== "";
+
+export const doesSameOptionExists = (newOption, options) =>
+  options.find((option) => option === newOption) !== undefined;
+
+export const doesCorrectOptionExists = (correctOption, options) =>
+  options.find((option) => option === correctOption) !== undefined;
