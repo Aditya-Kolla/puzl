797e62e970e0599387d5311139d142719705fa1b
Aditya Kolla <aditya.169108011@muj.manipal.edu>
2021-06-12 12:47:14 +0530

    Integrate player join lobby func

--- puzl-server/handlers/playerHandler.js
+++ puzl-server/handlers/playerHandler.js
@@ -1,7 +1,19 @@
+const { addPlayerToGame } = require('../cache/game');
+
 module.exports = (io, socket) => {
     const playerDisconnect = () => {
         console.log(`Disconnected ${socket.id}`);
     }
-  
-    socket.on("disconnect", playerDisconnect);
+ 
+    const playerJoin = (data) => {
+        const { playerId, gameId, nickname } = data;
+        console.log(data);
+        if(!gameId) return;
+        socket.join(gameId);
+        addPlayerToGame(gameId, playerId);
+        io.to(gameId).emit('player-add', nickname);
+    }
+
+    socket.on('disconnect', playerDisconnect);
+    socket.on('player-join', playerJoin);
   }
